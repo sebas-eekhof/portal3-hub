40f133dfa73cdbf74446f99563a8e0e6dfe0a9ba
Sebastiaan Eekhof <sebastiaan@jsmecommerce.nl>
2021-05-26 00:52:55 +0200

    a

--- index.js
+++ index.js
@@ -15,9 +15,19 @@ const init = async ({console}) => {
     
     Gpio.playEffect('status_led', 'wave', 1)
 
-    const socket = io.connect(_.get(process.env, 'SOCKET_SERVER', 'wss://portal3.nl:7474'), { transports: ['websocket'], reconnect: true, query: { hub_serial: await Device.GetSerialNumber(), model: await Device.getModel() }, maxReconnectionAttempts: Infinity })
+    const socket = io(_.get(process.env, 'SOCKET_SERVER', 'wss://portal3.nl:7474'), { transports: ['websocket'], reconnect: true, query: { hub_serial: await Device.GetSerialNumber(), model: await Device.getModel() }, maxReconnectionAttempts: Infinity })
 
     console.log('Starting to connect to socket')
+
+    socket.connect();
+
+    socket.on('reconnect', (attempt) => {
+        console.log(`Reconnecting to socket, attempt (${attempt})`)
+    })
+
+    socket.on('reconnect_error', (e) => {
+        console.error(`Received socket error: ${e.message}`)
+    })
 
     socket.on('disconnect', () => {
         console.log('Disconnected from socket')

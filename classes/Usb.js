540706f739addaba4443d1a5d56be9c6fbd422d1
Sebastiaan Eekhof <sebastiaan@jsmecommerce.nl>
2021-05-26 19:47:04 +0200

    a

--- classes/Usb.js
+++ classes/Usb.js
@@ -24,9 +24,7 @@ const getDeviceInfo = async (device) => {
     device.open();
     const interfaces = device.interfaces;
     const descriptor = device.deviceDescriptor;
-
     let device_info = {}
-    
     if(descriptor.bDeviceClass === 0 && _.get(interfaces, '[0]', false)) {
         device_info = {
             class: _.get(DeviceClasses.interface, `[${interfaces[0].descriptor.bInterfaceClass}].name`, null),
@@ -40,18 +38,15 @@ const getDeviceInfo = async (device) => {
             protocol: _.get(DeviceClasses.device, `[${descriptor.bDeviceClass}][${descriptor.bDeviceSubClass}][${descriptor.bDeviceProtocol}].name`, null),
         }
     }
-    
     const device_info_str = _.get(interfaces, '[0].descriptor.iInterface', false) ? await getStringDescriptor(device, interfaces[0].descriptor.iInterface) : null;
-
     const data = {
         name: await getStringDescriptor(device, descriptor.iProduct),
         manufacturer: await getStringDescriptor(device, descriptor.iManufacturer),
         serial_number: await getStringDescriptor(device, descriptor.iSerialNumber),
+        vendor_id: descriptor.idVendor,
+        product_id: descriptor.idProduct,
         device_info,
-        device_info_str,
-        descriptor,
-        interface: interfaces[0],
-        interface_extra: interfaces[0].descriptor.extra.toString()
+        device_info_str
     }
     device.close();
     return data;
@@ -59,9 +54,10 @@ const getDeviceInfo = async (device) => {
 
 const getDevices = async () => {
     const devices = doBlacklist(usb.getDeviceList());
-    console.log(await getDeviceInfo(devices[0]))
-    console.log('\n\n\n')
-    console.log(await getDeviceInfo(devices[1]))
+    let device_infos = [];
+    for(let i = 0; i < devices.length; i++)
+        device_infos.push(await getDeviceInfo(devices[i]))
+    console.log(device_infos)
     return true;
 };
 

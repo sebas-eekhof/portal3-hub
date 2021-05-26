d802042b957f44aa380a66b269cf949feed7ae41
Sebastiaan Eekhof <sebastiaan@jsmecommerce.nl>
2021-05-26 19:53:48 +0200

    a

--- classes/Usb.js
+++ classes/Usb.js
@@ -41,12 +41,13 @@ const getDeviceInfo = async (device) => {
     const device_info_str = _.get(interfaces, '[0].descriptor.iInterface', false) ? await getStringDescriptor(device, interfaces[0].descriptor.iInterface) : null;
     const data = {
         name: await getStringDescriptor(device, descriptor.iProduct),
-        manufacturer: await getStringDescriptor(device, descriptor.iManufacturer),
+        // manufacturer: await getStringDescriptor(device, descriptor.iManufacturer),
         // serial_number: await getStringDescriptor(device, descriptor.iSerialNumber),
         vendor_id: descriptor.idVendor,
         product_id: descriptor.idProduct,
         device_info,
-        device_info_str
+        device_info_str,
+        descriptor
     }
     // device.close();
     return data;

49c49cc096da3eb73ac0b230129227843ae0d681
Sebastiaan Eekhof <sebastiaan@jsmecommerce.nl>
2021-05-26 21:47:44 +0200

    a

--- classes/Hid.js
+++ classes/Hid.js
@@ -6,8 +6,8 @@ const getDevices = () => HID.devices();
 const onData = function(device) {
     const emitter = new EventEmitter();
     const dev = new HID.HID(device.path);
-    dev.on('data', data => pipe.emit('data', data))
-    dev.on('error', error => pipe.emit('error', error))
+    dev.on('data', data => emitter.emit('data', data))
+    dev.on('error', error => emitter.emit('error', error))
 
     return {
         pipe: emitter,

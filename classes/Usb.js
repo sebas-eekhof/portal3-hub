d27e8e39c0c7087970586717e444077640dfa971
Sebastiaan Eekhof <sebastiaan@jsmecommerce.nl>
2021-05-26 18:00:25 +0200

    a

--- classes/Usb.js
+++ classes/Usb.js
@@ -16,7 +16,7 @@ const doBlacklist = (devices) => devices.filter(device => !(vendorBlackList.incl
 const getDevices = async () => {
     const devices = doBlacklist(usb.getDeviceList());
     const test_device = devices[0];
-    test_device.open()
+    // test_device.open()
     const capabilities = await new Promise((resolve, reject) => test_device.getBosDescriptor((err, resp) => (err) ? reject(err) : resolve(resp)));
     console.log(capabilities)
     return true;

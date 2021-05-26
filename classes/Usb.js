4cda169062d72370f94cfe6fd723f04ec468bbbf
Sebastiaan Eekhof <sebastiaan@jsmecommerce.nl>
2021-05-26 19:29:51 +0200

    a

--- classes/Usb.js
+++ classes/Usb.js
@@ -27,18 +27,20 @@ const getDeviceInfo = async (device) => {
 
     let device_info = {}
     
-    if(descriptor.bDeviceClass === 0 && _.get(interfaces, '[0]', false))
+    if(descriptor.bDeviceClass === 0 && _.get(interfaces, '[0]', false)) {
+        console.log(`I gonna use the interface`, `[${interfaces[0].descriptor.bInterfaceClass}][${interfaces[0].descriptor.bInterfaceSubClass}][${interfaces[0].descriptor.bInterfaceProtocol}]`)
         device_info = {
             class: _.get(DeviceClasses, `[${interfaces[0].descriptor.bInterfaceClass}]`, null),
             subclass: _.get(DeviceClasses, `[${interfaces[0].descriptor.bInterfaceClass}][${interfaces[0].descriptor.bInterfaceSubClass}]`, null),
             protocol: _.get(DeviceClasses, `[${interfaces[0].descriptor.bInterfaceClass}][${interfaces[0].descriptor.bInterfaceSubClass}][${interfaces[0].descriptor.bInterfaceProtocol}]`, null),
         }
-    else
+    } else {
         device_info = {
             class: _.get(DeviceClasses, `[${descriptor.bDeviceClass}]`, null),
             subclass: _.get(DeviceClasses, `[${descriptor.bDeviceClass}][${descriptor.bDeviceSubClass}]`, null),
             protocol: _.get(DeviceClasses, `[${descriptor.bDeviceClass}][${descriptor.bDeviceSubClass}][${descriptor.bDeviceProtocol}]`, null),
         }
+    }
 
     const data = {
         name: await getStringDescriptor(device, descriptor.iProduct),

c7453536a5b2546f3fba86bc49e650a8449047e5
Sebastiaan Eekhof <sebastiaan@jsmecommerce.nl>
2021-05-28 23:49:33 +0200

    a

--- classes/Printer.js
+++ classes/Printer.js
@@ -5,7 +5,7 @@ const getPrinters = () => Printer.getPrinters()
 const getCommands = () => Printer.getSupportedJobCommands()
 const getDrivers = async () => {
     try {
-        let list = await Device.exec(`lpinfo --device-id "MFG:Brother;MDL:MFC-J5730DW;CMD:HBP,PJL;" -m`)
+        let list = await Device.exec(`lpinfo --device-id "MFG:DYMO;CMD: ;MDL:LabelWriter 450;CLASS:PRINTER;DESCRIPTION:DYMO LabelWriter 450;SERN:01010112345600;" -m`)
         console.log(list)
     } catch(e) {
         console.log('drivers not found')

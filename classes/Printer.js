7156d08341df90c1704c35ae08e3cc241f2aa463
Sebastiaan Eekhof <sebastiaan@jsmecommerce.nl>
2021-05-28 23:48:26 +0200

    a

--- classes/Printer.js
+++ classes/Printer.js
@@ -5,7 +5,7 @@ const getPrinters = () => Printer.getPrinters()
 const getCommands = () => Printer.getSupportedJobCommands()
 const getDrivers = async () => {
     try {
-        let list = await Device.exec(`lpinfo --make-and-model "Brother MFC" --product "L2750DW" -m`)
+        let list = await Device.exec(`lpinfo --device-id "MFG:Brother;MDL:MFC-J5730DW;CMD:HBP,PJL;" -m`)
         console.log(list)
     } catch(e) {
         console.log('drivers not found')

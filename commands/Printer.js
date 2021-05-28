988b43dfc9619094bf963712ba977322bf4d605d
Sebastiaan Eekhof <sebastiaan@jsmecommerce.nl>
2021-05-28 16:35:53 +0200

    a

--- commands/Printer.js
+++ commands/Printer.js
@@ -1,8 +1,5 @@
 const Printer = require('../classes/Printer');
 
 module.exports = {
-    getPrinter: Printer.getPrinter,
-    getPrinters: Printer.getPrinters,
-    printDirect: Printer.printDirect,
-    printFile: Printer.printFile
+    
 }

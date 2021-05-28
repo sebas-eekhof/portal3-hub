4fb1eab5e70270db93d632bb4223ffbe035f3b07
Sebastiaan Eekhof <sebastiaan@jsmecommerce.nl>
2021-05-28 23:00:06 +0200

    a

--- classes/Printer.js
+++ classes/Printer.js
@@ -3,25 +3,8 @@ const Device = require('./Device');
 
 const getPrinters = () => Printer.getPrinters()
 const getCommands = () => Printer.getSupportedJobCommands()
-const getDrivers = async () => {
-    let drivers = await Device.exec('lpinfo -m')
-    drivers = drivers.split('\n')
-    drivers.pop()
-    drivers.pop()
-    let list = [];
-    for(let i = 0; i < drivers.length; i++) {
-        const split_space = drivers[i].split(' ');
-        list.push({
-            path: split_space[0],
-            brand: split_space[1],
-            model: split_space[2]
-        })
-    }
-    console.log(list)
-}
 
 module.exports = {
     getPrinters,
-    getCommands,
-    getDrivers
+    getCommands
 }

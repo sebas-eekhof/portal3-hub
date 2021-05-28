594922763f7bd5e90608d6498b3178a203c82b99
Sebastiaan Eekhof <sebastiaan@jsmecommerce.nl>
2021-05-29 00:23:29 +0200

    a

--- classes/Printer.js
+++ classes/Printer.js
@@ -5,15 +5,30 @@ const _ = require('lodash');
 const getPrinters = () => Printer.getPrinters()
 const getCommands = () => Printer.getSupportedJobCommands()
 const getDevices = async () => {
-    let list = await Device.spawn('lpinfo', ['-l', '-v'])
-    // let list = await Device.exec('cat test.txt');
+    // let list = await Device.spawn('lpinfo', ['-l', '-v'])
+    let list = await Device.exec('cat test.txt');
     list = list.split('Device: ').filter(i => i.length !== 0)
     let devices = [];
     for(let i = 0; i < list.length; i++) {
         const info_rules = list[i].match(/(\w+) = ([^\s]+)/g);
-        console.log(info_rules)
+        let info = {
+            uri: null,
+            class: null,
+            info: null,
+            model: null,
+            id: null
+        }
+        for(let i = 0; i < info_rules.length; i++) {
+            const rule = info_rules[i].split(' = ');
+            if(rule.length === 2)
+                if(typeof info[rule[0]] !== "undefined")
+                    info[rule[0]] = rule[1];
+        }
+        if(info.id)
+            devices.push(info)
     }
-    // console.log(list)
+    console.log(devices)
+    // return devices;
 }
 const getDrivers = async () => {
     try {

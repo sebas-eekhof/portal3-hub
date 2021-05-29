dad46fb4425650739ab7f7fd9e839963710656c9
Sebastiaan Eekhof <sebastiaan@jsmecommerce.nl>
2021-05-29 18:18:57 +0200

    a

--- classes/Printer.js
+++ classes/Printer.js
@@ -8,33 +8,37 @@ let allDevices = [];
 
 const start_discovery = () => {
     const run = async () => {
-        let list = await Device.spawn('lpinfo', ['-l', '-v'])
-        list = list.split('Device: ').filter(i => i.length !== 0)
-        let devices = [];
-        for(let i = 0; i < list.length; i++) {
-            const info_rules = list[i].match(/(\w+) = ([^\s]+[ ]?)+/g);
-            let info = {
-                uri: null,
-                class: null,
-                info: null,
-                model: null,
-                id: null
+        try {
+            let list = await Device.spawn('lpinfo', ['-l', '-v'])
+            list = list.split('Device: ').filter(i => i.length !== 0)
+            let devices = [];
+            for(let i = 0; i < list.length; i++) {
+                const info_rules = list[i].match(/(\w+) = ([^\s]+[ ]?)+/g);
+                let info = {
+                    uri: null,
+                    class: null,
+                    info: null,
+                    model: null,
+                    id: null
+                }
+                for(let i = 0; i < info_rules.length; i++) {
+                    const rule = info_rules[i].split(' = ');
+                    if(rule.length === 2)
+                        if(typeof info[rule[0]] !== "undefined") {
+                            let val = rule[1];
+                            if(val.slice(-1) === '')
+                                val = val.substr(0, -1);
+                            info[rule[0]] = val;
+                        }
+                }
+                if(info.id && info.class !== 'file')
+                    devices.push(info)
             }
-            for(let i = 0; i < info_rules.length; i++) {
-                const rule = info_rules[i].split(' = ');
-                if(rule.length === 2)
-                    if(typeof info[rule[0]] !== "undefined") {
-                        let val = rule[1];
-                        if(val.slice(-1) === '')
-                            val = val.substr(0, -1);
-                        info[rule[0]] = val;
-                    }
-            }
-            if(info.id && info.class !== 'file')
-                devices.push(info)
+            allDevices = devices;
+            setTimeout(run, 5000)
+        } catch(e) {
+            setTimeout(run, 100)
         }
-        allDevices = devices;
-        setTimeout(run, 5000)
     }
     run();
 }

d35715e59fa6a80ee6beaad3c101fd6b66b8d2ba
Sebastiaan Eekhof <sebastiaan@jsmecommerce.nl>
2021-06-02 02:42:53 +0200

    com

--- classes/DMesg.js
+++ classes/DMesg.js
@@ -1,10 +1,26 @@
 const { spawn } = require('child_process');
+const _ = require('lodash');
+
+const DMESG_TYPE = Object.freeze({
+    printer: 'printer',
+    storage: 'storage',
+    scanner: 'scanner'
+})
 
 const startFollow = () => {
     const process = spawn(`dmesg`, [`-wH`]);
     let i = 0;
     process.stdout.on(`data`, data => {
-        console.log(i, data.toString())
+        const line = data.toString()
+        const time_rest = line.split('] ')
+        const message = line.replace(time_rest[0], '');
+        const id_message = message.split(': ');
+        
+        const data = {
+            identifier: id_message[0],
+            message: message.replace(`${id_message[0]}: `, ``)
+        }
+        console.log(i, data)
         i++;
     })
 }

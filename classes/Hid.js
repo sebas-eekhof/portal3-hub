eb749afd077ac5ac887ca9fb4723c6427346e209
Sebastiaan Eekhof <sebastiaan@jsmecommerce.nl>
2021-06-03 19:43:34 +0200

    com

--- classes/Hid.js
+++ classes/Hid.js
@@ -23,20 +23,6 @@ const streamDevice = ({out, onError}, {device}) => {
     }
 }
 
-const streamDevice = function(device) {
-    const emitter = new EventEmitter();
-    const dev = new HID.HID(device.path);
-    dev.on('data', data => emitter.emit('data', data.toString('utf-8').trim().replace(/\r?\n|\r/g, '').replace(' ', '').trim()))
-    dev.on('error', error => emitter.emit('error', error))
-
-    return {
-        pipe: emitter,
-        close: () => {
-            dev.close();
-        }
-    }
-}
-
 module.exports = {
     getDevices,
     streamDevice

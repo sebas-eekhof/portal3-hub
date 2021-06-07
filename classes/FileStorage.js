0aa9e37ad3fb02e5cc4db3c2b34d287f78ed8b52
Sebastiaan Eekhof <sebastiaan@jsmecommerce.nl>
2021-06-07 22:53:05 +0200

    com

--- classes/FileStorage.js
+++ classes/FileStorage.js
@@ -242,7 +242,7 @@ const streamFormatDrive = ({out, onError, kill}, { drive, name = 'usb', fstype =
             out({done: false, msg: 'Data verwijderen', step, total_steps});
 
             const size = (quick ? 20000000 : drive.size);
-            await new Promise(resolve => {
+            await new Promise((resolve, reject) => {
                 const process = child_process.spawn(`dd if=/dev/zero count=1 bs=${size} status=noxfer | pv -n -s ${size} > ${drive.path}`)
                 process.stdout.on('data', data => {
                     console.log(`Data: ${data.toString()}`)

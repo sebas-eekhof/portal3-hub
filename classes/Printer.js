0011d0098831c7350d653fab2d672bd0696c98ed
Sebastiaan Eekhof <sebastiaan@jsmecommerce.nl>
2021-06-09 17:56:54 +0200

    com

--- classes/Printer.js
+++ classes/Printer.js
@@ -74,6 +74,8 @@ const getPrinterDevice = async (uri) => {
     const setup_device = getSetupPrinters().find(i => i.options['device-uri'] === uri);
     const all_devices = await getAllDevices();
     connected_device = all_devices.find(i => i.uri === uri);
+    console.log(`Need uri ${uri}`)
+    console.log(all_devices)
     console.log(connected_device)
     let ret = {
         ...connected_device,

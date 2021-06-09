0ec0e7f98ad3a901b8283f17c6a07517bc89cd4c
Sebastiaan Eekhof <sebastiaan@jsmecommerce.nl>
2021-06-09 17:29:12 +0200

    com

--- classes/Printer.js
+++ classes/Printer.js
@@ -86,7 +86,9 @@ const getPrinters = async () => {
     let printers = getSetupPrinters()
     let list = [];
     for(let i = 0; i < printers.length; i++) {
-        const device_info = await getPrinterDevice(printers[i].options['device-uri']);
+        try {
+            const device_info = await getPrinterDevice(printers[i].options['device-uri']);
+        } catch(e) {console.error(e)}
         if(device_info.setup && typeof device_info.setup_device.name === "string" && device_info.setup_device.name.length !== 0)
             list.push(device_info)
     }

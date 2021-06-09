9d410f5afbc1cb0b6e525179c4a6c62ca4fc2b03
Sebastiaan Eekhof <sebastiaan@jsmecommerce.nl>
2021-06-09 21:56:38 +0200

    com

--- classes/User.js
+++ classes/User.js
@@ -1,6 +1,6 @@
 const linuxUser = require('linux-sys-user').promise();
 
-const getUsers = () => linuxUser.getUsers();
+const getUsers = () => linuxUser.getUsers().then(users => users.filter(user => user.homedir.includes('/portal3/storage')));
 
 module.exports = {
     getUsers
